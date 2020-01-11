286e54311624278016625a056ad956e72cb3aac5
Alexey Sokolov <alezx311@gmail.com>
2020-01-11 10:14:55 +0200

    Delete config.js

--- bot/config/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  database_url: "mongodb+srv://alezx311:bSx437Sd!@defaultcluster-ot2ca.mongodb.net/test?retryWrites=true&w=majority",
-  bot_api: "929018255:AAFJrKZ4Aq5YBuOgSmMcwtgvktE-wdHP1ro"
-}
