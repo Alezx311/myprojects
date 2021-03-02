5868dd1e73c373752c73150bdafdb0813e2f0048
alezx311 <alezx311@gmail.com>
2021-03-02 12:11:04 +0200

    update

---
+++ server/routes/search.js
@@ -0,0 +1,16 @@
+const Router = require('express').Router()
+
+const TYPES = ['bliss', 'sound']
+const blissPath = './public/images/bliss/'
+const samplesPath = './public/sounds/samples'
+
+Router.get('/', (req, res) => {
+  const query = req.query
+  const [[type, data]] = Object.entries(query)
+
+  if (!TYPES.includes(type)) {
+    res.status(500).send()
+  }
+})
+
+module.exports = Router
--- server/server.js
+++ server/server.js
@@ -3,16 +3,19 @@ require('dotenv').config()
 const express = require('express')
 const ecstatic = require('ecstatic')
 const http = require('http')
+const searchRouter = require('./routes/search')
 const { PORT } = process.env
 
 const app = express()
 
 app.use(
+  '/',
   ecstatic({
     root: `${__dirname}/public`,
     showDir: true
   })
 )
+app.use('/search', searchRouter)
 
 http.createServer(app).listen(PORT)
 
