9c4efe88744074f8b0b0c2f39f45f492daee059b
Oleksii Sokolov <alezx311@gmail.com>
2021-08-15 03:48:08 +0300

    FIX | Fix some errors on new PC, its still working!

--- client/src/components/Guitar/index.js
+++ client/src/components/Guitar/index.js
@@ -1,109 +1,109 @@
 import React from 'react'
 import * as Tone from 'tone'
-import { GUITAR_TUNINGS, TUNING_NAMES, NOTES, SCALES, SYNTHS, INSTRUMENTS } from '../constants'
-import { Random, Note, Sound } from '../helpers'
+import { GUITAR_TUNINGS, TUNING_NAMES, NOTES, SCALES, INSTRUMENTS } from '../constants'
+import { Random, Note } from '../helpers'
 import { Box, Button, Text, DropButton } from 'grommet'
 let synth
 
 const DropSelect = ({ label, value, options, onClick }) => {
-  const dropContent = options.map(v => <Button key={v} label={v} onClick={() => onClick(v)} />)
-  return (
-    <DropButton label={`${label}: ${value}`} dropAlign={{ top: 'bottom', right: 'right' }} dropContent={dropContent} />
-  )
+	const dropContent = options.map(v => <Button key={v} label={v} onClick={() => onClick(v)} />)
+	return (
+		<DropButton label={`${label}: ${value}`} dropAlign={{ top: 'bottom', right: 'right' }} dropContent={dropContent} />
+	)
 }
 
 export const Guitar = props => {
-  const { state, reducer } = props
+	const { state, reducer } = props
 
-  const opened = GUITAR_TUNINGS[state.tuning]
-  const octaved = opened.map(v => Random.noteStep(v, 12))
-  const stringNotes = [...opened, ...octaved].filter((v, i) => i < state.strings).reverse()
+	const opened = GUITAR_TUNINGS[state.tuning]
+	const octaved = opened.map(v => Random.noteStep(v, 12))
+	const stringNotes = [...opened, ...octaved].filter((v, i) => i < state.strings).reverse()
 
-  const Fretboard = () =>
-    stringNotes.map(open => (
-      <Box key={open} direction="row" gap="small">
-        {Random.noteSteps(open, state.frets).map(note => (
-          <Button
-            key={note}
-            size="small"
-            label={note}
-            onClick={() => {
-              reducer({ rootNote: note })
-              synth.triggerAttackRelease(note, '4n')
-            }}
-          />
-        ))}
-      </Box>
-    ))
-  const RiffView = () => (
-    <>
-      <Text hidden={!state.riff.length}>{state.riff.filter((v, i) => i < 10).join(' -> ')}</Text>
-      <Text>{JSON.stringify(state.valueOnPlay, null, '\t')}</Text>
-    </>
-  )
-  const RiffPlay = () => {
-    const onPlay = () => {
-      const notes = state.riff.map(v => Random.noteValues(v))
+	const Fretboard = () =>
+		stringNotes.map(open => (
+			<Box key={open} direction='row' gap='small'>
+				{Random.noteSteps(open, state.frets).map(note => (
+					<Button
+						key={note}
+						size='small'
+						label={note}
+						onClick={() => {
+							reducer({ rootNote: note })
+							synth.triggerAttackRelease(note, '4n')
+						}}
+					/>
+				))}
+			</Box>
+		))
+	const RiffView = () => (
+		<div>
+			<Text hidden={!state.riff.length}>{state.riff.filter((v, i) => i < 10).join(' -> ')}</Text>
+			<Text>{JSON.stringify(state.valueOnPlay, null, '\t')}</Text>
+		</div>
+	)
+	const RiffPlay = () => {
+		const onPlay = () => {
+			const notes = state.riff.map(v => Random.noteValues(v))
 
-      const sequence = new Tone.Sequence((time = Tone.now(), { note, duration, velocity }) => {
-        reducer({ valueOnPlay: { note, duration, velocity }, isPlaying: true })
+			const sequence = new Tone.Sequence((time = Tone.now(), { note, duration, velocity }) => {
+				reducer({ valueOnPlay: { note, duration, velocity }, isPlaying: true })
 
-        synth.triggerAttackRelease(note, '4n', time, velocity)
-      }, notes).start(1)
+				synth.triggerAttackRelease(note, '4n', time, velocity)
+			}, notes).start(1)
 
-      Tone.Transport.set({ bpm: 130, humanize: true, playbackRate: 1.3 })
-      Tone.Transport.start('+0.1')
-    }
-    const onStop = () => {
-      Tone.Transport.stop(0)
+			Tone.Transport.set({ bpm: 130, humanize: true, playbackRate: 1.3 })
+			Tone.Transport.start('+0.1')
+		}
+		const onStop = () => {
+			Tone.Transport.stop(0)
 
-      reducer({ isPlaying: false })
-    }
+			reducer({ isPlaying: false })
+		}
 
-    return (
-      <>
-        <Button disabled={state.isPlaying} label="Play" onClick={onPlay} />
-        <Button disabled={!state.isPlaying} label="Stop" onClick={onStop} />
-      </>
-    )
-  }
+		return (
+			<div>
+				<Button disabled={state.isPlaying} label='Play' onClick={onPlay} />
+				<Button disabled={!state.isPlaying} label='Stop' onClick={onStop} />
+			</div>
+		)
+	}
 
-  const SetupFretboard = () => (
-    <>
-      <DropSelect
-        label="Strings"
-        value={state.strings}
-        options={[4, 5, 6, 7, 8]}
-        onClick={v => reducer({ strings: v })}
-      />
-      <DropSelect label="Frets" value={state.frets} options={[12, 21, 24]} onClick={v => reducer({ frets: v })} />
-      <DropSelect label="Tuning" value={state.tuning} options={TUNING_NAMES} onClick={v => reducer({ tuning: v })} />
-    </>
-  )
-  const SetupRiff = () => (
-    <>
-      <DropSelect label="Root Note" value={state.rootNote} options={NOTES} onClick={v => reducer({ rootNote: v })} />
-      <DropSelect label="Scale" value={state.scale} options={SCALES} onClick={v => reducer({ scale: v })} />
-      <DropSelect
-        label="Melody Size"
-        value={state.size}
-        options={[10, 20, 50, 100]}
-        onClick={v => reducer({ size: v })}
-      />
-      <DropSelect
-        label="Sound Instrument"
-        value={state.instrumentName}
-        options={Object.keys(INSTRUMENTS)}
-        onClick={v => {
-          const urlEntries = Object.entries(INSTRUMENTS[v]).map(([key, val]) => [key, `/samples/${v}/${val}`])
-          const samples = Object.fromEntries(urlEntries)
+	const SetupFretboard = () => (
+		<div>
+			<DropSelect
+				label='Strings'
+				value={state.strings}
+				options={[4, 5, 6, 7, 8]}
+				onClick={v => reducer({ strings: v })}
+			/>
+			<DropSelect label='Frets' value={state.frets} options={[12, 21, 24]} onClick={v => reducer({ frets: v })} />
+			<DropSelect label='Tuning' value={state.tuning} options={TUNING_NAMES} onClick={v => reducer({ tuning: v })} />
+		</div>
+	)
+	const SetupRiff = () => (
+		<div>
+			<DropSelect label='Root Note' value={state.rootNote} options={NOTES} onClick={v => reducer({ rootNote: v })} />
+			<DropSelect label='Scale' value={state.scale} options={SCALES} onClick={v => reducer({ scale: v })} />
+			<DropSelect
+				label='Melody Size'
+				value={state.size}
+				options={[10, 20, 50, 100]}
+				onClick={v => reducer({ size: v })}
+			/>
+			<DropSelect
+				label='Sound Instrument'
+				value={state.instrumentName}
+				options={Object.keys(INSTRUMENTS)}
+				onClick={v => {
+					const urlEntries = Object.entries(INSTRUMENTS[v]).map(([key, val]) => [key, `/samples/${v}/${val}`])
+					const samples = Object.fromEntries(urlEntries)
 
-          synth = new Tone.Sampler(samples).toDestination()
+					synth = new Tone.Sampler(samples).toDestination()
 
-          reducer({ synthName: null, instrumentName: v })
-        }}
-      />
-      {/* <DropSelect
+					reducer({ synthName: null, instrumentName: v })
+				}}
+			/>
+			{/* <DropSelect
         label="Sound Synth"
         value={state.synthName}
         options={SYNTHS}
@@ -112,35 +112,35 @@ export const Guitar = props => {
           updateSynth()
         }}
       /> */}
-    </>
-  )
-  const SetupButtons = () => (
-    <>
-      <Button
-        disabled={!state.rootNote}
-        label="Generate Riff"
-        onClick={() => {
-          const { rootNote, scale, size } = state
-          const riff = Note.melody(rootNote, scale, size)
+		</div>
+	)
+	const SetupButtons = () => (
+		<div>
+			<Button
+				disabled={!state.rootNote}
+				rel='Generate Riff'
+				onClick={() => {
+					const { rootNote, scale, size } = state
+					const riff = Note.melody(rootNote, scale, size)
 
-          reducer({ riff })
-        }}
-      />
-    </>
-  )
-  const SetupGuitar = () => (
-    <Box direction="row" align="center" gap="medium">
-      <SetupFretboard />
-      <SetupRiff />
-      <SetupButtons />
-    </Box>
-  )
-  return (
-    <Box direction="column" align="center" gap="medium">
-      <SetupGuitar />
-      <Fretboard />
-      <RiffView />
-      <RiffPlay />
-    </Box>
-  )
+					reducer({ riff })
+				}}
+			/>
+		</div>
+	)
+	const SetupGuitar = () => (
+		<Box direction='row' align='center' gap='medium'>
+			<SetupFretboard />
+			<SetupRiff />
+			<SetupButtons />
+		</Box>
+	)
+	return (
+		<Box direction='column' align='center' gap='medium'>
+			<SetupGuitar />
+			<Fretboard />
+			<RiffView />
+			<RiffPlay />
+		</Box>
+	)
 }
--- client/src/components/Player/index.js
+++ client/src/components/Player/index.js
@@ -1,20 +1,22 @@
+import React from 'react'
 import { Box, Button, Text } from 'grommet'
-import { SYNTHS, NOTES, INSTRUMENTS } from '../constants'
+import { SYNTHS, NOTES } from '../constants'
 
 const Synths = props =>
-  SYNTHS.map(label => (
-    <Box key={label} direction="row" align="center" gap="small">
-      <Notes />
-      <Text>{label}</Text>
-      <Button size="small" label="Melody" />
-    </Box>
-  ))
+	SYNTHS.map(label => (
+		<Box key={label} direction='row' align='center' gap='small'>
+			<Notes />
+			<Text>{label}</Text>
+			<Button size='small' label='Melody' />
+		</Box>
+	))
+
 const Notes = props => NOTES.map(label => <Button key={label} label={label} />)
 
 export const Player = props => {
-  return (
-    <Box direction="column" align="center" gap="medium">
-      <Synths />
-    </Box>
-  )
+	return (
+		<Box direction='column' align='center' gap='medium'>
+			<Synths />
+		</Box>
+	)
 }
